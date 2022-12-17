e95293297b286417ffa51ec41202c52a6f305ab2
martinsbicudo <victor_bicudo@outlook.com>
2022-12-17 19:56:35 -0300

    feat: create Container Component feat: add Cora favicon chore(typescript): add @/* alias

---
+++ components/Container/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 0 15px;
+  max-width: 1024px;
+  margin: 0 auto;
+`
+
+export default Container
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import Head from 'next/head'
 
+import { Container } from '@/components'
+
 const Home = () => {
   return (
     <>
       <Head>
         <title>Cora Case</title>
+        <meta name="description" content="Cora Case" />
+        <link rel="icon" href="favicon.png" />
       </Head>
-      <main>CORA</main>
+      <main>
+        <Container>Cora</Container>
+      </main>
     </>
   )
 }
